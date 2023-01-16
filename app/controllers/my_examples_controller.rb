440fcc5a239d3af28d6d68c2059ccbb0b60edab0
dallinforsyth <dallfor192@gmail.com>
2023-01-16 11:22:52 -0600

    adding if statement to produce a singel fortune

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,5 +1,12 @@
 class MyExamplesController < ApplicationController
   def fortune_method
-    render json: { message: "hello", message2: "hello2", message3: "hello3" }
+    index = rand(3)
+    if index == 0
+    render json:  { fortune1: "You will lose your left shoe", }
+   elsif index == 1
+    render json:  { fortune2: "hello2" }
+   elsif index == 2
+    render json:  { fortune3: "hello3" }
+   end
   end
 end
