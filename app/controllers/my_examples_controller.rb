a37fbf0aac47fd0a9e72c0d01d39c00d181962fe
dallinforsyth <dallfor192@gmail.com>
2023-01-16 11:24:34 -0600

    added 3 fortunes to the page

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -2,11 +2,11 @@ class MyExamplesController < ApplicationController
   def fortune_method
     index = rand(3)
     if index == 0
-    render json:  { fortune1: "You will lose your left shoe", }
+    render json:  { fortune1: "You will lose your left shoe.", }
    elsif index == 1
-    render json:  { fortune2: "hello2" }
+    render json:  { fortune2: "You may find a nickel on the ground." }
    elsif index == 2
-    render json:  { fortune3: "hello3" }
+    render json:  { fortune3: "You may find a tickel me elmo on the ground." }
    end
   end
 end
